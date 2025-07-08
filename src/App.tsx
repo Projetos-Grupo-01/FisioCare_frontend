23b4ddc4d5cc178e43304ca58d16712d25458ef7
Samuel <samueldjsferreira@gmail.com>
2025-07-08 17:17:40 -0300

    :sparkles: Criação da pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+        <BrowserRouter>
+          {/* <Navbar /> */}
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          {/* <Footer /> */}
+        </BrowserRouter>
+    </>
   )
 }
 
