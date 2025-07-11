5f9ba8ce323d16a3d8d70ea6c2b86b184d8c431a
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-11 12:26:21 -0300

    :sparkles: Implementação página Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,35 @@
+import { useContext } from "react";
+import ListarExercicios from "../../components/exercicios/listarexercicios/ListarExercicios"
+import { AuthContext } from "../../contexts/AuthContext";
 
 
 function Home() {
+
+  const { usuario } = useContext(AuthContext);
+
   return (
-    <div>
-        <div>
-            <h1>Seja bem vindo!</h1>
-            <img src="" alt="" />
+    <>
+      <div className="flex justify-center bg-[#001427]">
+        <div className="container grid grid-cols-1 sm:grid-cols-2 text-white py-9">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <h2 className="text-5xl font-semibold">
+              Seja bem vindo <br />
+              {usuario.nome}!
+            </h2>
+          </div>
+
+          <div className="flex justify-center">
+            <img
+              src="homeImage.png"
+              alt="Imagem da página Home"
+              className="w-2/3 max-w-96 py-9"
+            />
+          </div>
         </div>
-    </div>
+      </div>
+
+      <ListarExercicios />
+    </>
   )
 }
 
